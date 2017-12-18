156c95f105290cf8cb3dec88d4350bc038d663f4
Karl Xu <karl.yogi.xu@gmail.com>
2017-12-18 22:46:20 +0800

    add database connection

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import pymysql.cursors
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return render_template('index.html')
+connection = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='',
+    db='test',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+@app.route('/db')
+def db():
+    with connection.cursor() as cursor:
+        sql = "select * from test"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        return jsonify(result)
+
+
 
 if __name__ == "__main__":
     app.run()
