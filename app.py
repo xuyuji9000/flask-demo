9450bf70487bf60293965d512bbc3a50b6b89352
Karl Xu <karl.yogi.xu@gmail.com>
2017-12-18 23:30:02 +0800

    extracted environment variables

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, current_app
 import pymysql.cursors
 
 app = Flask(__name__)
 
+app.config.from_pyfile('.env')
+
+
 connection = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='',
-    db='test',
-    charset='utf8mb4',
+    host=app.config['DATABASE_HOST'],
+    user=app.config['DATABASE_USER'],
+    password=app.config['DATABASE_PASSWORD'],
+    db=app.config['DATABASE_NAME'],
+    charset=app.config['DATABASE_CHARSET'],
     cursorclass=pymysql.cursors.DictCursor
 )
 
@@ -24,6 +27,11 @@ def db():
         cursor.execute(sql)
         result = cursor.fetchone()
         return jsonify(result)
+
+@app.route('/test')
+def test():
+    return app.config['DATABASE_HOST']
+
 
 
 
